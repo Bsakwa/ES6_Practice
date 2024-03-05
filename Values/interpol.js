15b86f0c199990e9c93bad6f12d9c4be71c974e8
Bsakwa <sakwabrian23@gmail.com>
2024-03-05 12:03:34 +0300

    Add a file to demonstrate interpolation in Javascript

---
+++ Values/interpol.js
@@ -0,0 +1,13 @@
+// We will do a demonstration of string interpolation in JS
+
+
+// String Interpolation
+let name = 'Sakwa';
+let age = 25;
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+// Output: My name is John and I am 25 years old.
+
+// We can also use template literals for string interpolation
+console.log(`The sum of 5 and 10 is ${5 + 10}`);
+// Output: The sum of 5 and 10 is 15
