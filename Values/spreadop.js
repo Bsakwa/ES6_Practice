d704781cc18d1b048e77970b9ec5ef37cf4b0945
Bsakwa <sakwabrian23@gmail.com>
2024-03-08 19:48:52 +0300

    Demonstrate spread operator in JS

---
+++ Values/spreadop.js
@@ -0,0 +1,10 @@
+// Demonstrating the spread operator in JS
+// Usage: node spreadop.js
+
+const arr1 = "Hello";
+
+const arr2 = [...arr1];
+
+console.log(arr2); // [ 'H', 'e', 'l', 'l', 'o' ]
+
+// Spread operator can be used to concatenate arrays
